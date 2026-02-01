3363625e382ab1a9969a479452e4be75b49a1669
dhanuramsdr <dhanuramseenivasan@gmail.com>
2026-02-01 04:15:57 -0500

    update

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const Connection = require('./config/db');
 const AllRouters = require('./route/allRoute');
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
