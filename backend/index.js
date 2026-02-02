99ff9f9da4298336852a08b92ad97d3f601592c7
dhanuramsdr <dhanuramseenivasan@gmail.com>
2026-02-02 03:58:01 -0500

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,13 @@ require('dotenv').config({ path: './config/.env' });
 const app = express();
 const Connection = require('./config/db');
 const AllRouters = require('./route/allRoute');
-
+ const allowedOrigins = [
+      'http://medicalfrontendweb.s3-website.eu-north-1.amazonaws.com',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+    ];
 app.use(cors({
-  origin: "*",
+  origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,7 @@ const environment = process.env.REACT_APP_ENV || 'development';
 
 const config = {
   development: {
-    API_URL: 'http://localhost:6050/api', // For local development
+    API_URL: 'http://51.21.93.236:6050/api', // For local development
   },
   production: {
     API_URL: 'http://your-load-balancer-dns-name/api', // Your ALB endpoint
